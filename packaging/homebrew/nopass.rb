6edb393391076732c2a8c9d11dd2479104d80d06
Sana <88476413+souravsspace@users.noreply.github.com>
2026-08-07 16:20:51 +0600

    Point the formula at v0.2.0

--- packaging/homebrew/nopass.rb
+++ packaging/homebrew/nopass.rb
@@ -5,8 +5,8 @@
 class Nopass < Formula
   desc "Fast, self-contained password manager"
   homepage "https://github.com/souravsspace/nopass"
-  url "https://github.com/souravsspace/nopass/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "57f0b92859fc64a564cd0c3f45f7b7a363fe00591289693a6d0e9a980fdbe10d"
+  url "https://github.com/souravsspace/nopass/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "51cd64f81e1187f5ef776bf5cc7aaa1841d63bd555fbd03b5c60b304d4f77790"
   license "MIT"
 
   depends_on "rust" => :build
